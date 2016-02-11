84eef67ac3eb4b572c0ddbf99a3e8b9fec2b5c5f
Colin Teal <the30yearswar@hotmail.com>
2016-02-11 09:15:35 -0800

    Add new gathering

--- src/clj/chili_dog_night/data.clj
+++ src/clj/chili_dog_night/data.clj
@@ -1,7 +1,52 @@
 (ns chili-dog-night.data
   (:require [hiccup.element :as el]))
 
-(def gatherings [{:date "2016/02/03"
+(def gatherings [{:date "2016/02/10"
+                  :address "3828 Meridian Ave N, Seattle, WA 98103, USA"
+                  :synopsis "February 10th, 2016: Sausages and Hot Dogs, Jackie & Ryan, and Insurgent."
+                  :food ["Sausages"
+                         "Smart Dogs Vegetarian Hot Dogs"
+                         "Pringles Pizza"
+                         "Chips Ahoy! Chewy Oreo Creme Filled"
+                         "Chips Ahoy! Chewy Chocolate Chip Cookies"]
+                  :attendees ["Alex Sanchez"
+                              "Greg Ryan"
+                              "Jason Aumann"
+                              "Jacob Dobner"
+                              "Colin Teal"
+                              "Kaia"]
+                  :media [{:title "Jackie & Ryan" :uri "http://www.imdb.com/title/tt3270108/"}
+                          {:title "Insurgent" :uri "http://www.imdb.com/title/tt2908446/"}]
+                  :notes [:p
+                          (el/link-to "http://www.nature.com/news/babylonian-astronomers-used-geometry-to-track-jupiter-1.19261" "Babylonian astronomers used geometry to track Jupiter")
+                          [:br]
+                          (el/link-to "https://en.wikipedia.org/wiki/Ancient_astronaut_hypothesis" "Ancient Astronauts")
+                          [:br]
+                          (el/link-to "http://www.newyorker.com/culture/cultural-comment/hello-again-the-weirdness-of-the-lcd-soundsystem-reunion" "LCD Soundsystem Reunion")
+                          [:br]
+                          (el/link-to "https://en.wikipedia.org/wiki/United_States_Postmaster_General" "The highest paid government official")
+                          [:br]
+                          (el/link-to "https://www.washingtonpost.com/news/federal-eye/wp/2015/08/24/millennials-working-in-government-are-at-their-lowest-levels-in-five-yearsnew-report-finds/" "Erosion of the government workforce")
+                          " (which might have to do in part with "
+                          (el/link-to "http://www.fbiagentedu.org/careers/fbi-special-agent/" "low pay")
+                          ")"
+                          [:br]
+                          (el/link-to "https://en.wikipedia.org/wiki/Chivalric_order" "Chivalric orders")
+                          " and "
+                          (el/link-to "https://en.wikipedia.org/wiki/The_Man_Who_Would_Be_King" "The Man Who Would Be King")
+                          [:br]
+                          (el/link-to "http://www.nytimes.com/2014/03/16/science/billionaires-with-big-ideas-are-privatizing-american-science.html" "Private equity supplants government")
+                          " while "
+                          (el/link-to "https://newrepublic.com/article/121752/trade-deals-give-corporations-power-intimidate-tiny-countries" "corporations morph into states")
+                          " ("
+                          (el/link-to "http://www.bloomberg.com/news/articles/2015-12-18/amazon-said-to-mull-leasing-planes-to-control-delivery-chain" "Amazon the new behemoth")
+                          ")"
+                          [:br]
+                          (el/link-to "http://www.bloomberg.com/news/articles/2015-12-18/amazon-said-to-mull-leasing-planes-to-control-delivery-chain")
+                          (el/link-to "https://en.wikipedia.org/wiki/Jeff_Bezos" "Jeff Bezos")
+                          " builds a "
+                          (el/link-to "http://www.10000yearclock.net" "10,000 Year Clock")]}
+                 {:date "2016/02/03"
                   :address "3828 Meridian Ave N, Seattle, WA 98103, USA"
                   :synopsis "February 3rd, 2016: Jake's Birthday, War Room, and Divergent."
                   :food ["Cheese and Pepperoni Pizza from Pagliacci"
