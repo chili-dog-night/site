f4e35f80fc19ea135a6e70ba352817a74adc6abc
Colin Teal <the30yearswar@hotmail.com>
2016-03-03 09:04:53 -0800

    Update Vagrant configuration and provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -2,10 +2,11 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/wily64"
 
   # Networking
   config.vm.network :forwarded_port, guest: 3000, host: 3000
+  config.vm.network :forwarded_port, guest: 5432, host: 5432
 
   config.vm.provider "virtualbox" do |v|
     v.name = "chili-dog-night"
--- provision.sh
+++ provision.sh
@@ -2,13 +2,20 @@
 
 add-apt-repository ppa:openjdk-r/ppa
 
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
 apt-get update
 apt-get upgrade -y
 
-wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+wget --quiet -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 apt-get remove -y --purge openjdk*
-apt-get install -y openjdk-8-jdk
+apt-get install -y openjdk-8-jdk postgresql-9.5
+
+update-ca-certificates -f
+
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
 
 cd /usr/local/bin
 curl -fsSLo boot https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh
