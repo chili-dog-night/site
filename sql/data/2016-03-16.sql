fea82fb49626f77592bf0d4090644f61c141e9b6
Colin Teal <the30yearswar@hotmail.com>
2016-03-17 21:32:34 -0700

    Actually fix data script

--- sql/data/2016-03-16.sql
+++ sql/data/2016-03-16.sql
@@ -7,5 +7,5 @@ INSERT INTO media
   created_at
 )
 VALUES
-('Pinocchio', 'http://www.imdb.com/title/tt2826774/', 1000, '2016-02-16', now()),
-('Extraction', 'http://www.imdb.com/title/tt4382872/', 1000, '2016-02-16', now())
+('Pinocchio', 'http://www.imdb.com/title/tt2826774/', 1000, '2016-03-16', now()),
+('Extraction', 'http://www.imdb.com/title/tt4382872/', 1000, '2016-03-16', now())
