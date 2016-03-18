c862c424b00a12b23bc6b97db1e0546e075d88ff
Colin Teal <the30yearswar@hotmail.com>
2016-03-17 21:30:08 -0700

    Fix data script

--- sql/data/2016-03-16.sql
+++ sql/data/2016-03-16.sql
@@ -8,4 +8,4 @@ INSERT INTO media
 )
 VALUES
 ('Pinocchio', 'http://www.imdb.com/title/tt2826774/', 1000, '2016-02-16', now()),
-('Extraction', 'http://www.imdb.com/title/tt4382872/', 1000, '2016-02-03', now())
+('Extraction', 'http://www.imdb.com/title/tt4382872/', 1000, '2016-02-16', now())
