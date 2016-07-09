5aff8b19e9e7a4d01169023255265907f8f30818
Colin Teal <the30yearswar@hotmail.com>
2016-07-09 00:40:33 -0700

    Fix syntax error in SQL script

--- sql/data/2016-07-06.sql
+++ sql/data/2016-07-06.sql
@@ -8,4 +8,4 @@ INSERT INTO media
 )
 VALUES
 ('The Scorch Trials', 'http://www.imdb.com/title/tt4046784/', 1000, '2016-07-06', now()),
-('Heaven\'s Door', 'http://www.imdb.com/title/tt2321269/', 1000, '2016-07-06', now())
+('Heaven''s Door', 'http://www.imdb.com/title/tt2321269/', 1000, '2016-07-06', now())
